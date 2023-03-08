0abca9541611eb739be4b2642f966c332de571c6
Zodo <malaizyogmuzumala@gmail.com>
2023-03-08 10:13:50 +0200

    bug fix

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,7 +3,7 @@ type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const testimonials: Array<Testimonials> = [];
+const testimonials: Array<Testimonials> = [{ name: "", quote: "" }];
 
 export default function Testimonials({ setSelectedPage }: Props) {
   return <section id="testimonials"> hello</section>;
