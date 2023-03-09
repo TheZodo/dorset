45970bba23d456a8137efe1da62cce9f22c096c4
Zodo <malaizyogmuzumala@gmail.com>
2023-03-09 11:35:17 +0200

    fixed

--- components/Loans.tsx
+++ components/Loans.tsx
@@ -51,7 +51,7 @@ const Loans = ({ setSelectedPage }: Props) => {
       >
         {/* HEADER */}
         <motion.div
-          className="md:my-5 md:w-3/5"
+          className="mx-8 md:mx-40 md:my-5 md:w-3/5"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -72,7 +72,7 @@ const Loans = ({ setSelectedPage }: Props) => {
         </motion.div>
         {/* LOANS */}
         <motion.div
-          className="mt-5 items-center justify-between gap-8 md:flex "
+          className="mx-8 mt-5 items-center justify-between gap-8 md:mx-40 md:flex "
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -97,7 +97,7 @@ const Loans = ({ setSelectedPage }: Props) => {
           <div>
             {/* Title */}
             <div className="relative">
-              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-absrtactwaves">
+              <div className="mx-10 before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-absrtactwaves md:mx-40">
                 <motion.div
                   initial="hidden"
                   whileInView="visible"
@@ -116,49 +116,51 @@ const Loans = ({ setSelectedPage }: Props) => {
               </div>
             </div>
             {/* Description */}
-            <motion.div
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ delay: 0.2, duration: 0.5 }}
-              variants={{
-                hidden: { opacity: 0, x: 50 },
-                visible: { opacity: 1, x: 0 },
-              }}
-            >
-              <p className="my-5">
-                <strong>DorsetPay</strong> is a type of short term financing
-                that allows consumers to make purchases from approved retailers
-                and pay for them at a future date, interest free.
-              </p>
-              <p className="mb-5">
-                <strong>DorsetPay</strong> makes larger value items more
-                affordable for shoppers as they can spread the cost of purchase
-                over a period of up to 90 days at no extra cost.
-              </p>
-              <p className="text-xl font-bold">Key features of DorsetPay:</p>
-              <ul>
-                <li className="list-disc">
-                  No interest to pay every order is entirely interest free, no
-                  matter the basket value or term of loan.
-                </li>
-                <li className="list-disc">No early repayment charges</li>
-                <li className="list-disc">
-                  Flexibility you can choose the period over which you want to
-                  spread the cost of your order.
-                </li>
-                <li className="list-disc">
-                  Fixed amounts monthly payments are fixed at the same amount
-                  over the term of your loan.
-                </li>
-              </ul>
-            </motion.div>
-            {/* Button */}
-            <div className="relative mt-16">
-              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
-                <ActionButton setSelectedPage={setSelectedPage}>
-                  Get a Loan
-                </ActionButton>
+            <div className="mx-10 md:mx-40">
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: 50 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                <p className="my-5">
+                  <strong>DorsetPay</strong> is a type of short term financing
+                  that allows consumers to make purchases from approved
+                  retailers and pay for them at a future date, interest free.
+                </p>
+                <p className="mb-5">
+                  <strong>DorsetPay</strong> makes larger value items more
+                  affordable for shoppers as they can spread the cost of
+                  purchase over a period of up to 90 days at no extra cost.
+                </p>
+                <p className="text-xl font-bold">Key features of DorsetPay:</p>
+                <ul>
+                  <li className="list-disc">
+                    No interest to pay every order is entirely interest free, no
+                    matter the basket value or term of loan.
+                  </li>
+                  <li className="list-disc">No early repayment charges</li>
+                  <li className="list-disc">
+                    Flexibility you can choose the period over which you want to
+                    spread the cost of your order.
+                  </li>
+                  <li className="list-disc">
+                    Fixed amounts monthly payments are fixed at the same amount
+                    over the term of your loan.
+                  </li>
+                </ul>
+              </motion.div>
+              {/* Button */}
+              <div className="relative mt-16">
+                <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Get a Loan
+                  </ActionButton>
+                </div>
               </div>
             </div>
           </div>
